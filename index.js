949e22c15293da8cf98df671cfede36292d70cc3
Lisa M <lamiller2200@gmail.com>
2019-09-24 11:20:46 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Scroll to a certain element
+//projects
+
+document.getElementById('projects-start').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
+
+document.getElementById('footer-start').scrollIntoView({ 
+    behavior: 'smooth' 
+  });
